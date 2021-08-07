ebdd9aae716946e62127352d20eef2ce9d2cc655
zeko9381 <janzajc@mailbox.org>
2021-08-07 16:46:34 +0200

    modified:   lights.py

--- lights.py
+++ lights.py
@@ -1,64 +1,32 @@
-from pyA20.gpio import gpio
-from pyA20.gpio import port
+from gpiozero import Button, PWMLED, Device
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-gpio.init() #Initialize module. Always called first
+lightSwitch = Button(14, bounce_time=0.01)
+light = PWMLED(18, initial_value = 0, frequency = 5000)
 
-#Konfiguracija
-switch_port = port.PA10
+def light_on(fade, fade_time = 1):
+    if fade:
+        for i in range(0, 255, 1):
+            light.value = i / 255
+            sleep(fade_time / 255)
+            print(light.value)
+        print("ON")
+    else:
+        light.value = 1
+    
+def light_off(fade, fade_time = 1):
+    if fade:
+        for i in range(255, 0, -1):
+            light.value = i / 255
+            sleep(fade_time / 255)
+            print(light.value)
+    else:
+        light.value = 0
+        
+# The lambdas are for passing arguments
+lightSwitch.when_pressed = lambda: light_on(False)
+lightSwitch.when_released = lambda: light_off(False)
 
-gpio.setcfg(switch_port, gpio.INPUT)
-#gpio.pullup(switch_port, 0)
-
-#PWM setup
-export = open("/sys/class/pwm/pwmchip0/export", "w")
-export.write("0")
-export.close()
-
-period = open("/sys/class/pwm/pwmchip0/pwm0/period", "w")
-period.write("500000")
-period.close()
-
-polarity = open("/sys/class/pwm/pwmchip0/pwm0/polarity", "w")
-polarity.write("normal")
-polarity.close()
-
-enable = open("/sys/class/pwm/pwmchip0/pwm0/enable", "w")
-enable.write("1")
-enable.close()
-
-state = gpio.input(switch_port)
-last_state = state
-
-try:
-    while True:
-        state = gpio.input(switch_port)
-
-        if state != last_state:
-            if state == 1:
-                for i in reversed(range(0, 200000, 100)):
-                    duty_cycle = open("/sys/class/pwm/pwmchip0/pwm0/duty_cycle", "w")
-                    duty_cycle.write(str(i))
-                    duty_cycle.close()
-
-            elif state == 0:
-                for i in range(0, 200000, 100):
-                    duty_cycle = open("/sys/class/pwm/pwmchip0/pwm0/duty_cycle", "w")
-                    duty_cycle.write(str(i))
-                    duty_cycle.close()
-
-        last_state = state
-        sleep(0.001) # Without this, the CPU gets pinned to 100%
-
-except KeyboardInterrupt:
-    print("Exiting...")
-
-    enable = open("/sys/class/pwm/pwmchip0/pwm0/enable", "w")
-    enable.write("0")
-    enable.close()
-
-    unexport = open("/sys/class/pwm/pwmchip0/unexport", "w")
-    unexport.write("0")
-    unexport.close()
-
-    print("Exited!")
+while True:
+    sleep(10)
